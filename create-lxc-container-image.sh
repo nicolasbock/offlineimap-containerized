e1560a45787a457673df4be174eb08c497ca3a2e
Nicolas Bock <nicolasbock@gmail.com>
2021-04-28 10:28:51 -0600

    Updated create container script
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- create-lxc-container-image.sh
+++ create-lxc-container-image.sh
@@ -3,9 +3,14 @@
 set -e -u -x
 
 CONTAINER=offlineimap-build-container
+ALIAS=offlineimap-python2
 
 if lxc info ${CONTAINER}; then
   lxc delete --force ${CONTAINER}
+fi
+
+if lxc info ${ALIAS}; then
+  lxc delete --force ${ALIAS}
 fi
 
 lxc launch ubuntu:bionic ${CONTAINER}
@@ -23,8 +28,8 @@ lxc exec ${CONTAINER} -- pip install six
 lxc exec ${CONTAINER} -- pip install rfc6555
 lxc exec ${CONTAINER} -- pip install offlineimap
 
-lxc file push ./offlineimap-entrypoint.sh ${CONTAINER}/var/tmp
+lxc file push ./offlineimap-entrypoint.sh ${CONTAINER}/var/tmp/
 
 lxc stop ${CONTAINER}
-lxc publish ${CONTAINER} --alias offlineimap-python2
+lxc publish ${CONTAINER} --alias ${ALIAS}
 lxc delete ${CONTAINER}
